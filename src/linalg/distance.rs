e0dbe5edcd6e4bea86e5fe5f92be9d230e5ddedd
Hitoshi Manabe <int.toshi13@gmail.com>
2021-08-21 17:09:03 +0900

    Add hamming distance

--- src/linalg/distance.rs
+++ src/linalg/distance.rs
@@ -15,6 +15,7 @@ pub fn calcurate_distance<T: FloatScalar>(distance_type: &DistanceType, v1: &[T]
 }
 
 pub fn compute_euclidean_distance<T: FloatScalar>(p1: &[T], p2: &[T]) -> Result<T, NNSearchError> {
+    // FXIME
     if p1.len() != p2.len() {
         return Err(NNSearchError::ValueError(format!("Inconsistent length: {} != {}", p1.len(), p2.len())))
     }
@@ -23,6 +24,22 @@ pub fn compute_euclidean_distance<T: FloatScalar>(p1: &[T], p2: &[T]) -> Result<
         val = val + (p1[i] - p2[i]) * (p1[i] - p2[i]);
     }
     Ok(val.sqrt())
+}
+
+pub fn compute_hamming_distance(p1: &[bool], p2: &[bool]) -> Result<u32, NNSearchError> {
+    // FIXME
+    if p1.len() != p2.len() {
+        return Err(NNSearchError::ValueError(format!("Inconsistent length: {} != {}", p1.len(), p2.len())))
+    }
+    // FIXME: parallelizable
+    let val =(0..p1.len()).fold(0, |dist, idx| {
+        if p1[idx] == p2[idx] {
+            dist
+        } else {
+            dist + 1
+        }
+    });
+    Ok(val)
 }
 
 #[cfg(test)]
@@ -43,5 +60,14 @@ mod tests {
         let v2 = vec![0.3];
         // FIXME: loosen check of error message.
         assert_eq!(compute_euclidean_distance(&v1, &v2).unwrap_err(), NNSearchError::ValueError("Inconsistent length: 2 != 1".to_string()));
+    }
+
+    #[test]
+    fn test_compute_hamming_distance() {
+        // 01001101
+        let v1 = vec![false, true, false, false, true, true, false, true];
+        // 10111100
+        let v2 = vec![true, false, true, true, true, true, false, false];
+        assert_eq!(compute_hamming_distance(&v1, &v2).unwrap(), 5);
     }
 }
